45d90a1bb13b82b28adc3420fbbcdcbbfce7dddd
DevJayz <jyy0047@gmail.com>
2022-02-23 05:20:29 +0900

    Fix-Function: 2022-02-23 05:20:29

--- src/main/java/hasune/prototype/ExampleResource.java
+++ src/main/java/hasune/prototype/ExampleResource.java
@@ -11,7 +11,7 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello World!! With AWS!";
     }
 }
 
